fb4efc36a5530769873f9df43636d835436ffff0
Imane <Imane@192.168.11.104>
2021-11-15 01:07:16 +0100

    Ajout EJB CustomerManager

---
+++ src/main/java/co/en/tpcustomer/session/CustomerManager.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package co.en.tpcustomer.session;
+
+import co.en.tpcustomerapplicationcohen.entities.Customer;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Imane
+ */
+@Stateless
+public class CustomerManager {
+@PersistenceContext(unitName = "customerPU") private EntityManager em;
+public List<Customer> getAllCustomers() {
+Query query = em.createNamedQuery("Customer.findAll"); return query.getResultList();
+}
+public Customer update(Customer customer) { return em.merge(customer);
+}
+public void persist(Customer customer) { em.persist(customer);
+} }
